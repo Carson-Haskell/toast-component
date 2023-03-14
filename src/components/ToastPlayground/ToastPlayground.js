484b674e9e0401d0d5a4decbb9b3e31a6f096d62
Carson Haskell <carson.haskell@gmail.com>
2023-03-14 15:14:14 -0700

    hook up form inputs to react state and add radio buttons

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,6 +7,9 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState('');
+  const [variant, setVariant] = React.useState('');
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,7 +27,11 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              id="message"
+              className={styles.messageInput}
+              onChange={(e) => setMessage(e.target.value)}
+            />
           </div>
         </div>
 
@@ -33,17 +40,20 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+            {VARIANT_OPTIONS.map((variantOption) => (
+              <label key={variantOption} htmlFor={`variant-${variantOption}`}>
+                <input
+                  id={`varient-${variantOption}`}
+                  type="radio"
+                  name="variant"
+                  value={variantOption}
+                  checked={variant === variantOption}
+                  onChange={(e) => setVariant(e.target.value)}
+                />
+                {variantOption}
+              </label>
+            ))}
+            {console.log(variant)}
           </div>
         </div>
 
